752cc01b7e0ee223fe4f47a73e240e7184045f61
yyyyyano <145003765+yyyyyano@users.noreply.github.com>
2024-11-29 23:09:25 +0300

    Add files via upload

---
+++ IteratorsTests.cpp
@@ -0,0 +1,137 @@
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiAdjacentVertexIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiEdgeIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiIncidentEdgeIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiVertexIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\NonorientedGraph.h"
+#include "pch.h"
+
+TEST(BiIncidentEdgeIteratorTests, IncidentEdgesExist) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    BiIncidentEdgeIterator<int> it(&graph, 1);
+    EXPECT_EQ(*it, make_tuple(1, 1, 2)); 
+    ++it;
+    EXPECT_EQ(*it, make_tuple(2, 1, 3));
+    ++it;
+    BiIncidentEdgeIterator<int> endIt(&graph, 1);
+    while (endIt != BiIncidentEdgeIterator<int>(&graph, 0)) {
+        ++endIt;
+    }
+    EXPECT_TRUE(it == endIt);  
+}
+
+TEST(BiIncidentEdgeIteratorTests, Incident_Throw) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    BiIncidentEdgeIterator<int> it(&graph, 5);
+    EXPECT_THROW(*it, out_of_range);
+}
+
+TEST(BiIncidentEdgeIteratorTests, DecrementTest) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    BiIncidentEdgeIterator<int> it(&graph, 1); 
+    EXPECT_EQ(*it, make_tuple(1, 1, 2)); 
+    ++it;
+    EXPECT_EQ(*it, make_tuple(2, 1, 3));  
+    --it; 
+    EXPECT_EQ(*it, make_tuple(1, 1, 2));
+    BiIncidentEdgeIterator<int> currIt(&graph, 1);
+    EXPECT_TRUE(it == currIt);
+    BiIncidentEdgeIterator<int> next_currIt(&graph, 3);
+    EXPECT_TRUE(it != next_currIt);
+}
+
+TEST(BiIncidentEdgeIteratorTest, NoIncidentEdges) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    graph.AddVertex(4);  
+    BiIncidentEdgeIterator<int> it(&graph, 4);
+    EXPECT_TRUE(it == BiIncidentEdgeIterator<int>(&graph, 4));  
+    EXPECT_TRUE(it != BiIncidentEdgeIterator<int>(&graph, 3));
+}
+
+TEST(BiAdjacentVertexIteratorTest, AdjacentVertices) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    graph.AddVertex(4);
+    BiAdjacentVertexIterator<int> it(&graph, 1);
+    EXPECT_EQ(*it, 3);  
+    ++it;
+    EXPECT_EQ(*it, 4);
+}
+
+TEST(BiAdjacentVertexIteratorTest, AdjacentVertices_Notadj) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    graph.AddVertex(4);
+    BiAdjacentVertexIterator<int> it(&graph, 4);
+    EXPECT_TRUE(it == BiAdjacentVertexIterator<int>(&graph, 4));
+    EXPECT_TRUE(it != BiAdjacentVertexIterator<int>(&graph, 2));
+}
+
+TEST(BiAdjacentVertexIteratorTest, DecrementAdjacentVertices) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddVertex(3);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(1, 3);
+    BiAdjacentVertexIterator<int> it(&graph, 1);
+    ++it; 
+    --it; 
+    EXPECT_EQ(*it, 3); 
+}
+
+TEST(BiAdjacentVertexIteratorTest, DereferenceVertexOutOfRangeByTarget) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddEdge(1, 3); 
+    BiAdjacentVertexIterator<int> it(&graph, 1);
+    EXPECT_THROW(*it, out_of_range);
+}
+
+TEST(BiAdjacentVertexIteratorTest, DereferenceEdgeOutOfRange) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddEdge(1, 2);
+    BiAdjacentVertexIterator<int> it(&graph, 1);
+    it.SetCurrentEdge(100); 
+    EXPECT_THROW(*it,out_of_range);
+}
+
+TEST(BiAdjacentVertexIteratorTest, DereferenceEdgeNegativeIndex) {
+    NonorientedGraph<int> graph;
+    graph.AddVertex(1);
+    graph.AddVertex(2);
+    graph.AddEdge(1, 2);
+    BiAdjacentVertexIterator<int> it(&graph, 1);
+    it.SetCurrentEdge(-1); 
+    EXPECT_THROW(*it, out_of_range);
+}
---
+++ NonorientedGraphTests.cpp
@@ -0,0 +1,429 @@
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiAdjacentVertexIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiEdgeIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiIncidentEdgeIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\BiVertexIterator.h"
+#include "E:\PPOIS\labs\lab4\lab4_PPOI\NonorientedGraph.h"
+#include "pch.h"
+
+TEST(NonorientedGraphTest, IsEmpty_True) {
+	NonorientedGraph<int> graph1;
+	EXPECT_TRUE(graph1.IsEmpty());
+}
+
+TEST(NonorientedGraphTest, IsEmpty_False) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	EXPECT_FALSE(graph1.IsEmpty());
+}
+
+TEST(NonorientedGraphTest, ClearTest) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	EXPECT_FALSE(graph1.IsEmpty());
+	graph1.ClearGraph();
+	EXPECT_TRUE(graph1.IsEmpty());
+	EXPECT_EQ(graph1.GetEdgesCount(), 0);
+	EXPECT_EQ(graph1.GetVertexCount(), 0);
+}
+
+TEST(NonorientedGraphTest, HasVertex_True) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	EXPECT_TRUE(graph1.HasVertex(11));
+}
+
+TEST(NonorientedGraphTest, HasVertex_¿Ù‰˚Û) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	EXPECT_FALSE(graph1.HasVertex(321));
+}
+
+TEST(NonorientedGraphTest, HasVertexByIdx_True) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	EXPECT_TRUE(graph1.HasVertexByIdx(1));
+}
+
+TEST(NonorientedGraphTest, HasVertexByIdx_False) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	EXPECT_FALSE(graph1.HasVertexByIdx(11));
+}
+
+TEST(NonorientedGraphTest, HasEdge_True_TwoCombinations) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	graph1.AddEdge(1, 3);
+	EXPECT_TRUE(graph1.HasEdge(1,2));
+	EXPECT_TRUE(graph1.HasEdge(2, 1));
+	EXPECT_TRUE(graph1.HasEdge(3, 1));
+}
+
+TEST(NonorientedGraphTest, HasEdge_False) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	graph1.AddEdge(1, 3);
+	EXPECT_FALSE(graph1.HasEdge(3, 2));
+}
+
+TEST(NonorientedGraphTest, HasEdgeByIdx_True) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	graph1.AddEdge(1, 3);
+	EXPECT_TRUE(graph1.HasEdgeByIdx(1));
+}
+
+TEST(NonorientedGraphTest, HasEdgeByidx_False) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(110);
+	EXPECT_NO_THROW(graph1.AddVertex(1), runtime_error);
+	graph1.AddVertex(11);
+	graph1.AddVertex(10);
+	graph1.AddEdge(1, 2);
+	graph1.AddEdge(1, 3);
+	EXPECT_FALSE(graph1.HasEdgeByIdx(3));
+}
+
+TEST(NonorientedGraphTest, AddVertex_CatchException) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(13);
+	ASSERT_THROW(graph1.AddVertex(13), runtime_error);
+}
+
+TEST(NonorientedGraphTest, DeleteVertex_CatchException) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(13);
+	EXPECT_THROW(graph1.DeleteVertex(2), runtime_error);
+}
+
+TEST(NonorientedGraphTest, DeleteVertex_NoException) {
+	NonorientedGraph<int> graph1;
+	graph1.AddVertex(13);
+	graph1.AddVertex(8);
+	graph1.AddEdge(1, 2);
+	graph1.AddEdge(2, 2);
+	EXPECT_EQ(graph1.GetVertexCount(), 2);
+	EXPECT_NO_THROW(graph1.DeleteVertex(2), runtime_error);
+    EXPECT_EQ(graph1.GetVertexCount(), 1);
+}
+
+TEST(NonorientedGraphTest, AddEdge_CatchException) {
+	NonorientedGraph<int> graph;
+	graph.AddVertex(10);
+	graph.AddVertex(2);
+	graph.AddVertex(32);
+	graph.AddEdge(1, 2);
+	ASSERT_THROW(graph.AddEdge(1, 2), runtime_error);
+}
+
+TEST(NonorientedGraphTest, AddEdge_CatchNoException) {
+	NonorientedGraph<int> graph;
+	graph.AddVertex(10);
+	graph.AddVertex(2);
+	graph.AddVertex(32);
+	graph.AddEdge(1, 2);
+	EXPECT_NO_THROW(graph.AddEdge(1, 3), runtime_error);
+}
+
+TEST(NonorientedGraphTest, DeleteEdge_CatchException) {
+	NonorientedGraph<int> graph;
+	graph.AddVertex(10);
+	graph.AddVertex(2);
+	graph.AddVertex(32);
+	graph.AddEdge(1, 2);
+	ASSERT_THROW(graph.DeleteEdge(1, 3), runtime_error);
+}
+
+TEST(NonorientedGraphTest, DeleteEdge_CatchNoException) {
+	NonorientedGraph<int> graph;
+	graph.AddVertex(10);
+	graph.AddVertex(2);
+	graph.AddVertex(32);
+	graph.AddEdge(1, 2);
+	graph.AddEdge(3, 2);
+	EXPECT_NO_THROW(graph.DeleteEdgeByIdx(2), runtime_error);
+	EXPECT_EQ(graph.GetEdgesCount(), 1);
+}
+
+TEST(NonorientedGraphtest, DeleteEdgeByidx_CatchException) {
+	NonorientedGraph<int> graph2;
+	graph2.AddVertex(10);
+	graph2.AddVertex(2);
+	graph2.AddVertex(32);
+	graph2.AddEdge(1, 2);
+	graph2.AddEdge(3, 2);
+	ASSERT_THROW(graph2.DeleteEdgeByIdx(3), runtime_error);
+}
+
+TEST(NonorientedGraph, GetVertexCount_Correct) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	EXPECT_EQ(gr.GetVertexCount(), 2);
+}
+
+TEST(NonorientedGraph, GetEdgesCount_Correct) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 1);
+	gr.AddEdge(1, 2);
+	EXPECT_EQ(gr.GetEdgesCount(), 2);
+}
+
+TEST(NonorientedGraphTests, CalculateVertexDegree_CatchException) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 1);
+	gr.AddEdge(1, 2);
+	EXPECT_THROW(gr.CalculateVertexDegree(3), runtime_error);
+}
+
+
+TEST(NonorientedGraphTests, CalculateVertexDegree_Correct) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 1);
+	gr.AddEdge(1, 2);
+	EXPECT_EQ(gr.CalculateVertexDegree(2), 1);
+}
+
+TEST(NonorientedGraphTests, CalculateEdgeDegree_CatchException) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 1);
+	EXPECT_THROW(gr.CalculateEdgeDegree(2), runtime_error);
+}
+
+
+TEST(NonorientedGraphTests, CalculateEdgeDegree_Correct) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 1);
+	EXPECT_EQ(gr.CalculateEdgeDegree(1), 1);
+}
+
+TEST(NonorientedGraphTests, CalculateEdgeDegree_CorrectNotPetlya) {
+	NonorientedGraph<int> gr;
+	gr.AddVertex(10);
+	gr.AddVertex(3);
+	gr.AddEdge(1, 2);
+	EXPECT_EQ(gr.CalculateEdgeDegree(1), 2);
+}
+
+TEST(NonorientedGraphTests, OperatorRavno) {
+	NonorientedGraph<int> a1;
+	a1.AddVertex(1);
+	a1.AddVertex(2);
+	a1.AddEdge(1, 2);
+	NonorientedGraph<int> b1;
+	b1 = a1;
+	EXPECT_EQ(b1.GetEdgesCount(), 2);
+	EXPECT_EQ(b1.GetVertexCount(), 2);
+}
+
+TEST(NonorientedGraphTests, CopyConstructor) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 2);
+	NonorientedGraph<int> b=a;
+	EXPECT_EQ(b.GetEdgesCount(), 1);
+	EXPECT_EQ(b.GetVertexCount(), 2);
+}
+
+TEST(NonorientedGraphTest, OperatorRavnoRavno_True) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	EXPECT_TRUE(a == b);
+}
+
+TEST(NonorientedGraphTest, OperatorRavnoRavno_False) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddVertex(3);
+	EXPECT_FALSE(a == b);
+}
+
+TEST(NonorientedGraphTest, OperatorNotRavno_True) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	a.AddEdge(2, 2);
+	EXPECT_TRUE(a != b);
+}
+
+TEST(NonorientedGraphTest, OperatorNotRavno_False) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	EXPECT_FALSE(a != b);
+}
+
+TEST(NonorientedGraphTests, OperatorLessTrue) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_TRUE(a < b);
+}
+
+TEST(NonorientedGraphTests, OperatorLessFalse) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddVertex(23);
+	b.AddVertex(33);
+	b.AddVertex(43);
+	EXPECT_FALSE(b < a);
+}
+
+TEST(NonorientedGraphTests, OperatorMoreTrue) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_TRUE(b>a);
+}
+
+TEST(NonorientedGraphTests, OperatorMoreFalse) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	a.AddEdge(1, 2);
+	EXPECT_FALSE(b > a);
+}
+
+TEST(NonorientedGraphTests, OperatorLessRavnoTrue) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_TRUE(a <= b);
+	a.AddEdge(1, 2);
+	EXPECT_TRUE(a <= b);
+}
+
+TEST(NonorientedGraphTests, OperatorLessRavnoFalse) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_FALSE(b <= a);
+	a.AddEdge(1, 2);
+	EXPECT_FALSE(b <= a);
+}
+
+TEST(NonorientedGraphTests, OperatorMoreRavnoTrue) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_TRUE(b >= a);
+	a.AddEdge(1, 2);
+	EXPECT_TRUE(b >= a);
+}
+
+TEST(NonorientedGraphTests, OperatorMoreRavnoFalse) {
+	NonorientedGraph<int> a;
+	a.AddVertex(1);
+	a.AddVertex(2);
+	a.AddEdge(1, 1);
+	NonorientedGraph<int> b;
+	b = a;
+	b.AddEdge(2, 2);
+	EXPECT_FALSE(a >= b);
+	a.AddEdge(1, 2);
+	EXPECT_FALSE(a >= b);
+}
+
+TEST(NonorientedGraphTests, Output_Correct) {
+	NonorientedGraph<int> g;
+	g.AddVertex(1);
+	g.AddVertex(2);
+	g.AddVertex(3);
+	g.AddEdge(1, 1);
+	g.AddVertex(12);
+	testing::internal::CaptureStdout();
+	cout << g;
+	string output = testing::internal::GetCapturedStdout();
+	string expected_output = "Vertices:\nv1 (1)\nv2 (2)\nv3 (3)\nv4 (12)\n\nEdges:\ne1: v1 - v1\n";
+	EXPECT_EQ(output, expected_output);
+}
+
+TEST(NonorientedGraphTests, PrintMatrix_Correct) {
+	NonorientedGraph<int> g;
+	g.AddVertex(1);
+	g.AddVertex(2);
+	g.AddVertex(3);
+	g.AddEdge(1, 1);
+	g.AddVertex(12);
+	testing::internal::CaptureStdout();
+	g.PrintIncidenceMatrix();
+	string output = testing::internal::GetCapturedStdout();
+	string expected_output = "   e1   \nv1  2    \nv2  0    \nv3  0    \nv4  0    \n";
+	EXPECT_EQ(output, expected_output);
+}
